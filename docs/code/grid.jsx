346fae491747918cf818459967b155f14ca5f1b6
Douglas Hanley <thesecretaryofwar@gmail.com>
2025-06-17 16:36:27 -0500

    fix up grid example

--- docs/code/grid.jsx
+++ docs/code/grid.jsx
@@ -1,22 +1,13 @@
 // draw a grid of square boxes filled in light gray. each box contains an arrow that is pointing in a particular direction. that direction rotates clockwise as we move through the grid.
-const head = new Arrow(90, {
-    pos: [0.5, 0],
-    head: 0.3,
-    tail: 1,
-});
-const arrows = linspace(0, 360, 10).slice(0, 9).map(
-    th => new Rotate(head, th)
-);
-const boxes = arrows.map(a => new Frame(a, {
-    aspect: 1,
-    border: 1,
-    rounded: 0.1,
-    padding: 0.2,
-    fill: '#EEE',
-}));
-const grid = new Grid(reshape(boxes, [3, 3]), {
-    spacing: 0.1,
-});
-return new Frame(grid, {
-    padding: 0.1,
-});
+const boxes = linspace(0, 360, 10).slice(0, 9).map(th =>
+  <Frame padding margin border rounded fill aspect={1}>
+    <Group rotate={th} invar>
+      <Arrow direc={0} tail={1} pos={[1, 0.5]} rad={0.5} />
+    </Group>
+  </Frame>
+)
+return <Frame padding>
+  <Grid rows={3} spacing>
+    {boxes}
+  </Grid>
+</Frame>
