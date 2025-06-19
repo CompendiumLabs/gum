276e7f91c4086d8f4e96fc92394d0d0108607562
Douglas Hanley <thesecretaryofwar@gmail.com>
2025-06-18 23:44:30 -0500

    tamed bar plot

--- docs/code/barplot.jsx
+++ docs/code/barplot.jsx
@@ -1,24 +1,8 @@
 // A plot with three bars with black borders at "A", "B", and "C". The first bar is red and is the shortest, the second bar is blue and is the tallest, while the third bar is gray.
-
-const abar = new Rect({
-  fill: red,
-});
-const bbar = new Rect({
-  fill: blue,
-});
-const bdat = [
-  ['A', [3, abar]],
-  ['B', [8, bbar]],
-  ['C', 6],
-];
-const bars = new BarPlot(bdat, {
-  ylim: [0, 10],
-  yticks: 6,
-  title: 'Example BarPlot',
-  xlabel: 'Category',
-  ylabel: 'Value',
-  bar_fill: '#AAA',
-});
-return new Frame(bars, {
-  margin: 0.3,
-});
+<Frame margin={0.25}>
+  <BarPlot ylim={[0, 10]} yticks={6} title="Example BarPlot" xlabel="Category" ylabel="Value">
+    <Bar label="A" size={3} fill={red} />
+    <Bar label="B" size={8} fill={blue} />
+    <Bar label="C" size={6} fill={gray} />
+  </BarPlot>
+</Frame>
